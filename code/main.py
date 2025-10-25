7503f0d7070a143b0302e89e5df65dc76cbb4537
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 11:40:41 +0530

    Create main.py

---
+++ code/main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello, World 👋")
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("🤖 Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
