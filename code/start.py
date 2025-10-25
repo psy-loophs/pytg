e7782005adcd50bbd454aeb74546ecc7fa655a82
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 11:58:26 +0530

    Update start.py

--- code/start.py
+++ code/start.py
@@ -3,4 +3,8 @@ from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello, from python 👋")
-  
+
+num=int(input("Enter a number"))
+async def calc(update: Update,context: ContextTypes.Default_Types):
+    await update.messsage.reply_text("Entered number is ",num)
+
