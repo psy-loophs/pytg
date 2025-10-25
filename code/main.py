59493693b7966aead42bc82c0848b4b33776628e
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 12:33:50 +0530

    Remove unused imports from main.py

--- code/main.py
+++ code/main.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-from start import start
-from calc import calc, handle_calc
 from handle import register_handles
+
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
