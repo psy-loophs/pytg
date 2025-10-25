374e6aa08c86584050717554cd50b04faa941ec6
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 12:29:58 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,16 +2,16 @@ import os
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from start import start
-from calc import calc#, handle_calc
 
+from start import start
+from calc import calc, handle_calc
+from handle import register_handles
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
     print("🤖 Bot is running...")
     app.run_polling()
 
