7d441d4ddacdaa649f3ede67efd1f96db851da95
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 20:42:30 +0530

    Update calc.py

--- code/calc.py
+++ code/calc.py
@@ -1,18 +1,19 @@
 from telegram import Update 
 from telegram.ext import ContextTypes
 
+user_waiting_for_number=None
 
 async def calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
-   # uid = update.message.from_user.id
+    uid = update.message.from_user.id
     text = update.message.text.strip()
 
 
     if text == "/calc":
-      #  user_waiting_for_number[uid] = True
+        user_waiting_for_number[uid] = True
         await update.message.reply_text("Enter a number:")
         return
 
-   # if uid in user_waiting_for_number:
+    if uid in user_waiting_for_number:
         if text.isdigit():
             num = int(text)
             await update.message.reply_text(f"You entered {num}. ✅")
