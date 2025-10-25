8fe5ae7cf37934a0a96bc2120308db88d076c698
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 20:28:10 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -17,7 +17,6 @@ def main():
     app.add_handler(CommandHandler("start", start))
   
     app.add_handler(CommandHandler("calc", calc))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_calc))
     
     
     print("🤖 Bot is running...")
