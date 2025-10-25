593c0f91fa8da15f26f4a9ec2ccada268b694cb6
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 12:08:52 +0530

    Create calc.py

---
+++ code/calc.py
@@ -0,0 +1,14 @@
+from telegram import Update 
+from telegram.ext import ContextTypes
+async def calc(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Enter a number: ")
+  
+
+async def handle_calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    if text.isdigit():
+        num = int(text)
+        await update.message.reply_text(f"You entered {num}.")
+    else:
+        await update.message.reply_text("❌ Please send a valid number.")
+      
