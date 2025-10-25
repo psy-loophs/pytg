7f2de0010659cb549070c72ed772b5a6cad84dd0
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 11:52:44 +0530

    Uncomment ContextTypes import in start.py

--- code/start.py
+++ code/start.py
@@ -1,5 +1,5 @@
 from telegram import Update 
-#from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello, World 👋")
