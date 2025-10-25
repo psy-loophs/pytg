cb2dd64710b4d537a213defdb698592412ee361a
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 20:09:02 +0530

    Update calc.py

--- code/calc.py
+++ code/calc.py
@@ -1,10 +1,10 @@
 from telegram import Update 
 from telegram.ext import ContextTypes
 
+num=0
 async def calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Enter a number: ")
-
-
+return num
 
 async def handle_calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
