805d5823d86f46771c9d79c304858b2f403f52b7
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 11:59:41 +0530

    Remove number input and calc function
    Removed input handling and calc function.

--- code/start.py
+++ code/start.py
@@ -4,7 +4,4 @@ from telegram.ext import ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello, from python 👋")
 
-num=int(input("Enter a number"))
-async def calc(update: Update,context: ContextTypes.Default_Types):
-    await update.messsage.reply_text("Entered number is ",num)
 
