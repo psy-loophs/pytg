50763401079a3e1c1d893d9fcb0f7f3744a235d4
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 11:50:43 +0530

    Update start.py

--- code/start.py
+++ code/start.py
@@ -1,4 +1,5 @@
 from telegram import update 
+from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello, World 👋")
