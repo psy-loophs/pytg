15862a91745a6bc28bfbd4b31976fd70a382e066
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-25 21:16:04 +0530

    Update calc.py

--- code/calc.py
+++ code/calc.py
@@ -20,6 +20,8 @@ async def calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
             del user_waiting_for_number[uid]  
         else:
             await update.message.reply_text("❌ Please send a valid number.")
+            user_waiting_for_number[uid] = True
+            await update.message.reply_text("Enter a number:")
             return
        
     
