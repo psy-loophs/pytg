720be55563f5ca8182643199963f0c54ffa1e883
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-26 20:45:51 +0530

    Update calc.py

--- code/calc.py
+++ code/calc.py
@@ -36,7 +36,7 @@ async def calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
   
     if uid in active_user:
       await update.message.reply_text("Enter second number:")
-      return 
+       
         
     if uid in active_user:
         if text.isdigit():
