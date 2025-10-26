cfb48c29481e2fa3bcce894f67f72d0395a65ab7
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-26 11:28:00 +0530

    Update calc.py

--- code/calc.py
+++ code/calc.py
@@ -19,14 +19,21 @@ async def calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
             num = int(text)
         else:
             await update.message.reply_text("❌ Please send a valid number.")
+            del user_waiting_for_number[uid] 
             return
-            user_waiting_for_number[uid] = True
-            await update.message.reply_text("Enter another number: ")
-            if text.isdigit():
+            
+    if user_waiting_for_number[uid] = True:
+        await update.message.reply_text("Enter another number: ")
+        
+    if uid in user_waiting_for_number:
+        if text.isdigit():
             num1=int(text)
             await update.message.reply_text(num + num1)
             
             del user_waiting_for_number[uid]  
+        
+            
+            
         else:
             await update.message.reply_text("❌ Please send a valid number.")
             user_waiting_for_number[uid] = True
