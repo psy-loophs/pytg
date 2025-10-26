880a745c7807da04b25035a89c8df38611b2e134
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-26 10:05:44 +0530

    Update start.py

--- code/start.py
+++ code/start.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
+    user_waiting_for_number={}
     uid = update.message.from_user.id  
     if uid in user_waiting_for_number:
         del user_waiting_for_number[uid]
