dfff34fd1d81cddfffaee857407a2abbea26932a
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-26 10:00:02 +0530

    Update start.py

--- code/start.py
+++ code/start.py
@@ -2,6 +2,10 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if uid in user_waiting_for_number:
+        del user_waiting_for_number[uid]
+        
     await update.message.reply_text("Hello, from python 👋")
 
 
