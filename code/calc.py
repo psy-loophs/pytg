93340515a819ccdccad59f9627a1a70c46cb2685
Jaydev Jayamon <161050824+psy-loophs@users.noreply.github.com>
2025-10-26 19:25:27 +0530

    Update calc.py

--- code/calc.py
+++ code/calc.py
@@ -42,7 +42,7 @@ async def calc(update: Update, context: ContextTypes.DEFAULT_TYPE):
         entered_num["number2"].append(num2)
         await update.message.reply_text("sum is",num1 + num2)
         del active_user[uid]
-        return
+        raise SystemExit
           
       else:
         await update.message.reply_text("❌ Please send a valid second number.")
